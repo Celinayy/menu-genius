092720f81c2cc2d37c3d5ab9ea03ff418e81895f
Celinay <vangel.celina@woit.hu>
2024-09-13 11:02:42 +0200

    feat: Navigate to random product

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,11 +9,24 @@ import {
   Typography,
   Grid2 as Grid,
   Box,
+  IconButton,
 } from "@mui/material";
 import { useRouter } from "next/router";
+import NextImage from "next/image";
+import { Image, Product } from "@prisma/client";
+import { trpc } from "@/trpc/client";
+import { StarRounded } from "@mui/icons-material";
 
 export default function Home() {
+  const { data, isLoading } = trpc.product.list.useQuery({});
+
+  const randomProduct = data
+    ? data[Math.floor(Math.random() * data?.length)]
+    : undefined;
   const router = useRouter();
+
+  const { data: product } = trpc.product.find.useQuery({});
+
   return (
     <Container>
       <HeaderComponent />
@@ -61,16 +74,37 @@ export default function Home() {
           </Card>
         </Grid>
         <Grid size={{ xs: 12, md: 6 }}>
-          <Card>
-            <CardContent>
-              <Typography>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Blanditiis doloremque quasi magni quisquam fugiat ex. Eveniet,
-                sunt perspiciatis. Earum eum pariatur doloremque nemo at vero
-                quasi totam repudiandae vel sunt.
-              </Typography>
-            </CardContent>
-          </Card>
+          {!!randomProduct && (
+            <Card>
+              <CardContent>
+                <Box display={"flex"} justifyContent={"center"}>
+                  <NextImage
+                    src={randomProduct.image.data}
+                    alt={randomProduct.name}
+                    width={randomProduct.image.width}
+                    height={randomProduct.image.height}
+                  />
+                </Box>
+                <Divider sx={{ margin: "12px" }} />
+                <Stack direction={"row"} justifyContent={"space-between"}>
+                  <Stack direction={"column"}>
+                    <Typography>{randomProduct.name}</Typography>
+                    <Typography>{randomProduct.price} EUR</Typography>
+                  </Stack>
+                  <Stack direction={"row"} spacing={1}>
+                    <Button
+                      variant="contained"
+                      onClick={() =>
+                        router.push(`/products/${randomProduct.id}/`)
+                      }
+                    >
+                      Irány a termék
+                    </Button>
+                  </Stack>
+                </Stack>
+              </CardContent>
+            </Card>
+          )}
         </Grid>
       </Grid>
     </Container>
